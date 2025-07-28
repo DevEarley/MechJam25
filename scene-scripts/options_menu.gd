7512cc8abb4b9bfaf34dd643a012f36f52b4eba9
w40krocks <alexjohnnston2005@gmail.com>
2025-07-28 17:31:16 +0100

    User Options file addition
    user option file which contains the difficulty chosen by the user, it is checked when loading the main scenes determines whether the difficulty menu or the main menu is shown, the difficulty can later be changed in the options menu

--- scene-scripts/options_menu.gd
+++ scene-scripts/options_menu.gd
@@ -1,6 +1,8 @@
 extends Node
 class_name OptionsMenu;
 
+var real_time : bool
+
 func _ready():
 	$save_button.connect("pressed",_on_save_button_pressed)
 	$difficulty_24_hours_button.connect("pressed",_on_difficulty_24_hours_button_pressed)
@@ -8,13 +10,13 @@ func _ready():
 
 
 func _on_difficulty_24_hours_button_pressed():
-	pass
+	real_time = true
 	#save to options user file
 	#get_tree().change_scene("res://scenes/main_menu.tscn")
 
 
 func _on_difficulty_instant_button_pressed():
-	pass
+	real_time = false
 	#save to options user file
 	#get_tree().change_scene("res://scenes/main_menu.tscn")
 
@@ -22,6 +24,8 @@ func _on_difficulty_instant_button_pressed():
 
 
 func _on_save_button_pressed():
-	pass
+	STATE.USER_REAL_TIME = real_time
+	DATA.save_user_data(STATE.USER_REAL_TIME)
+	print("Saved!")
 	#save to options user file
 	#get_tree().change_scene("res://scenes/main_menu.tscn")
