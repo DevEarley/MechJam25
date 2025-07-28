aed6e49477984a2e5b2b2d22908650dce5a6ebf2
w40krocks <alexjohnnston2005@gmail.com>
2025-07-28 11:38:26 +0100

    Update options_menu.gd

--- scene-scripts/options_menu.gd
+++ scene-scripts/options_menu.gd
@@ -1,6 +1,8 @@
 extends Node
 class_name OptionsMenu;
 
+var real_time : bool
+
 func _ready():
 	$save_button.connect("pressed",_on_save_button_pressed)
 	$difficulty_24_hours_button.connect("pressed",_on_difficulty_24_hours_button_pressed)
@@ -14,7 +16,7 @@ func _on_difficulty_24_hours_button_pressed():
 
 
 func _on_difficulty_instant_button_pressed():
-	pass
+	real_time = false
 	#save to options user file
 	#get_tree().change_scene("res://scenes/main_menu.tscn")
 
@@ -22,6 +24,18 @@ func _on_difficulty_instant_button_pressed():
 
 
 func _on_save_button_pressed():
-	pass
+	real_time = true
 	#save to options user file
 	#get_tree().change_scene("res://scenes/main_menu.tscn")
+
+
+func _save_difficulty_choice():
+	var config_user = ConfigFile.new()
+	var err = await config_user.load("user://user_data.cfg")
+	if err == 7: #if file doesn't exist, load base mission list, and then save it user side
+		config_user.load("res://data/user_data.cfg")
+		config_user.set_value("USER","USER_REAL_TIME",real_time)
+		config_user.save("user://user_data.cfg")
+	else:
+		config_user.set_value("USER","USER_REAL_TIME",real_time)
+		config_user.save("user://user_data.cfg")
